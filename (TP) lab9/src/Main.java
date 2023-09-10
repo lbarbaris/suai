8e33a59a4fd8bd0a8554ca94c710a6e16069dc41
lbarbaris <144600282+lbarbaris@users.noreply.github.com>
2023-09-10 17:17:07 +0300

    Delete (TP) lab9 directory

--- (TP) lab9/src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-
-    public static void main(String args[]) {
-        Deadlock.Thread1 T1 = new Deadlock.Thread1();
-        Deadlock.Thread2 T2 = new Deadlock.Thread2();
-        T1.start();
-        T2.start();
-    }
-}
